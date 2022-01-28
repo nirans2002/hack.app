ace1714a5156c95758010dd11eedaff482537b4c
newton2149 <navneetsasikumar@gmail.com>
2022-01-28 15:46:49 +0530

    "firebase added"

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
