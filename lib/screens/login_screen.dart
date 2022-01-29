aa8b48215e701d2abc89041cecb02d5c87805835
jaidan22 <jaidansleeba22@gmail.com>
2022-01-29 10:49:48 +0530

    updated Login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,11 +5,21 @@ class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State createState() => _LoginScreen();
+  _LoginScreen createState() => _LoginScreen();
 }
 
 class _LoginScreen extends State<LoginScreen> {
   String dropdownvalue = 'Student';
+
+  final _textU = TextEditingController();
+  final _textP = TextEditingController();
+  bool _validateU = false;
+  bool _validateP = false;
+  void dispose() {
+    _textU.dispose();
+    _textP.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,86 +30,92 @@ class _LoginScreen extends State<LoginScreen> {
     ];
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
-          margin: const EdgeInsets.fromLTRB(20, 8, 20, 8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Title
-              const Center(
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26,
-                  ),
+        margin: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Title
+            const Center(
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
                 ),
               ),
+            ),
 
-              // Form
-              Form(
-                child: Column(
-                  children: <Widget>[
-                    // Dropdown
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
-                      child: DropdownButton(
-                        isExpanded: true,
-                        value: dropdownvalue,
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownvalue = newValue!;
-                          });
-                          print(dropdownvalue);
-                        },
-                        items: items.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items),
-                          );
-                        }).toList(),
-                      ),
+            // Form
+            Form(
+              child: Column(
+                children: <Widget>[
+                  // Dropdown
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+                    child: DropdownButton(
+                      borderRadius: BorderRadius.circular(8),
+                      isExpanded: true,
+                      value: dropdownvalue,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue = newValue!;
+                        });
+                      },
+                      items: items.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
                     ),
-                    // Username
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+                  ),
+                  // Username
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+                    child: TextFormField(
+                      controller: _textU,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Username',
+                        errorText: _validateU ? 'Username is required' : null,
+                      ),
+                      validator: (value) {
+                        if (value == null) {
+                          return "Please enter some value";
+                        }
+                      },
+                    ),
+                  ),
+
+                  // Password
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
                       child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Username',
+                        controller: _textP,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          labelText: 'Password',
+                          errorText: _validateP ? 'Password is required' : null,
                         ),
                         validator: (value) {
                           if (value == null) {
                             return "Please enter some value";
                           }
                         },
-                      ),
-                    ),
-
-                    // Password
-                    Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 8),
-                        child: TextFormField(
-                          obscureText: true,
-                          enableSuggestions: false,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                          ),
-                          validator: (value) {
-                            if (value == null) {
-                              return "Please enter some value";
-                            }
-                          },
-                        )),
-                  ],
-                ),
+                      )),
+                ],
               ),
+            ),
 
-              // Login button
+            // Login button
+            if (dropdownvalue != 'Admin')
               Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -109,10 +125,21 @@ class _LoginScreen extends State<LoginScreen> {
                     style: TextStyle(fontSize: 16),
                   ),
                   onPressed: () {
-                    Navigator.pushNamed(context, '/home');
+                    setState(() {
+                      _textU.text.isEmpty
+                          ? _validateU = true
+                          : _validateU = false;
+                      _textP.text.isEmpty
+                          ? _validateP = true
+                          : _validateP = false;
+                    });
+                    if (!_validateU && !_validateP) {
+                      Navigator.pushNamed(context, '/home');
+                    }
                   },
                 ),
               ),
+            if (dropdownvalue != 'Admin')
               const Center(
                 child: Text(
                   'OR',
@@ -122,22 +149,32 @@ class _LoginScreen extends State<LoginScreen> {
                 ),
               ),
 
-              // Login with Google
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: ElevatedButton(
-                  child: const Text(
-                    'Login with Google',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home');
-                  },
+            // Login with Google
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ElevatedButton(
+                child: const Text(
+                  'Login with Google',
+                  style: TextStyle(fontSize: 16),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _textU.text.isEmpty
+                        ? _validateU = true
+                        : _validateU = false;
+                    _textP.text.isEmpty
+                        ? _validateP = true
+                        : _validateP = false;
+                  });
+                  if (!_validateU && !_validateP) {
+                    Navigator.pushNamed(context, '/home');
+                  }
+                },
               ),
+            ),
 
-              // Signup option
+            // Signup option
+            if (dropdownvalue != 'Admin')
               Center(
                 child: RichText(
                   text: TextSpan(
@@ -159,8 +196,9 @@ class _LoginScreen extends State<LoginScreen> {
                   ),
                 ),
               ),
-            ],
-          )),
+          ],
+        ),
+      ),
     );
   }
 }
